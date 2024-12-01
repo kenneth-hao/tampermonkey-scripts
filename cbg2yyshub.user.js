f82bc45de1f1bb47fb8f0f9258502dfc9bce23b6
haoyw <18510318685@163.com>
2024-12-01 15:20:31 +0800

    remove auto open new tab 4 yyshub

--- cbg2yyshub.user.js
+++ cbg2yyshub.user.js
@@ -3,7 +3,7 @@
 // @namespace    yyshub.top
 // @description  直接加载 CBG 数据到痒痒鼠魔方。
 // @license      GPT
-// @version      2024-11-29
+// @version      2024-12-01
 // @author       清明月见
 // @homepage     http://yyshub.top/
 // @iconURL      http://yyshub.top/static/img/favicon.png
@@ -15,10 +15,6 @@
 // @grant        unsafeWindow
 // @grant        GM_setValue
 // @grant        GM_addValueChangeListener
-// @grant        GM_getTab
-// @grant        GM_saveTab
-// @grant        GM_getTabs
-// @grant        GM_openInTab
 // @downloadURL https://update.greasyfork.org/scripts/518093/%E7%97%92%E7%97%92%E9%BC%A0%E9%AD%94%E6%96%B9%20Helperjs.user.js
 // @updateURL https://update.greasyfork.org/scripts/518093/%E7%97%92%E7%97%92%E9%BC%A0%E9%AD%94%E6%96%B9%20Helperjs.meta.js
 // ==/UserScript==
@@ -34,45 +30,20 @@
 
     console.log('load Tampermonkey of 痒痒鼠魔方 Helper');
 
-    GM_getTab(function(tab) {
-        tab.href = unsafeWindow.location.href;
-        GM_saveTab(tab);
-    });
-
-    if (unsafeWindow.location.href.startsWith(CBG_URL)) {
-        let checkTimeout;
-        clearTimeout(checkTimeout);
-        checkTimeout = setTimeout(() => {
-            GM_getTabs((tabs) => {
-                let isOpenYyshub = false;
-                for (const [tabId, tab] of Object.entries(tabs)) {
-                    if (tab?.href?.includes(YYSHUB_URL)) {
-                        isOpenYyshub = true;
-                        break; 
-                    }
-                }
-    
-                if (!isOpenYyshub) {
-                    GM_openInTab(YYSHUB_URL + '/#/yuhun/list', { 
-                        active: false, 
-                        insert: true, 
-                        setParent: true 
-                    });
-                }
-            });
-        }, 500); // 500ms 的防抖延时
-    }
-
-    GM_addValueChangeListener(KEY_OF_CBG_DATA, function(name, old_value, new_value, remote) {
-        if (remote && new_value) {
-            // console.log(name, old_value, new_value, remote)
-            elmGetter.get('#yys4tampermonkey').then(e => {
-                console.log('# 魔方 # 加载数据')
-                e.click(new_value);
-            });
+    if (unsafeWindow.location.href.startsWith(YYSHUB_URL)) {
+        console.log('load 4 YYSHub');
+        GM_addValueChangeListener(KEY_OF_CBG_DATA, function(name, old_value, new_value, remote) {
+            if (remote && new_value) {
+                // console.log(name, old_value, new_value, remote)
+                elmGetter.get('#yys4tampermonkey').then(e => {
+                    console.log('# 魔方 # 加载数据')
+                    e.click(new_value);
+                });
+            }
             GM_setValue(KEY_OF_CBG_DATA, '');
-        }
-    })
+            console.log('GM_setValue reset to EMPTY success');
+        })
+    }
 
     const loadMofang = (data) => {
         if (data) {
@@ -90,6 +61,7 @@
                         b.style.color = "rgb(236, 209, 107)";
                         b.onclick = function() {
                             GM_setValue(KEY_OF_CBG_DATA, data) // open close
+                            console.log('GM_setValue success');
                         }
                         const navbar = navbarMenu.parentElement;
                         navbar.insertBefore(b, navbar.children[0]);
@@ -99,23 +71,26 @@
         }
     }
 
-    ajaxHooker.hook(request => {
-        if (request.url.startsWith(CBG_DESC_URL_MATCH)) {
-            request.response = res => {
-                if (res.status == 200) {
-                    loadMofang(res.json)
-                }
-            };
-        }
-        if (request.url.startsWith(CBG_DETAIL_URL_MATCH)) {
-            request.response = res => {
-                if (res.status == 200) {
-                    const data = JSON.parse(res.responseText)
-                    if (data?.equip?.equip_desc) {
-                        loadMofang(data)
+    if (unsafeWindow.location.href.startsWith(CBG_URL)) {
+        console.log('load 4 CBG');
+        ajaxHooker.hook(request => {
+            if (request.url.startsWith(CBG_DESC_URL_MATCH)) {
+                request.response = res => {
+                    if (res.status == 200) {
+                        loadMofang(res.json)
+                    }
+                };
+            }
+            if (request.url.startsWith(CBG_DETAIL_URL_MATCH)) {
+                request.response = res => {
+                    if (res.status == 200) {
+                        const data = JSON.parse(res.responseText)
+                        if (data?.equip?.equip_desc) {
+                            loadMofang(data)
+                        }
                     }
                 }
             }
-        }
-    })
+        })
+    }
 })();
